438c91da1b6fa0b911797f8d603ad82a0fdc3fda
harsh <66548698+harsssh@users.noreply.github.com>
2023-11-25 04:36:32 +0900

    show message on deployment

--- commands.sh
+++ commands.sh
@@ -1,9 +1,13 @@
 #!/bin/bash -eux
 
 repo="/home/isucon/isucon-template"
-app_service="isucon.go"
+app="isucon.go"
 
 function deploy() {
+  # show commit info
+  echo "Current Branch: $(cd $repo git && rev-parse --abbrev-ref HEAD)" && \
+  echo "Last Commit Message: $(cd $repo && git log -1 --pretty=%B)"
+
   # WARN: .gitkeep is also added to /.gitkeep
   find "$repo/config/all" -type f | while read -r src; do
     dst="/${src#$repo/config/all}"
@@ -20,16 +24,19 @@ function deploy() {
   # (cd "$repo/webapp/go" && go build -o app)
 
   if [ "$HOSTNAME" = "s1" ]; then
-    sudo systemctl restart {nginx,mysql,"$app_service"}
+    sudo systemctl restart {nginx,mysql,"$app"} && echo -e "\e[32mRestarted:\e[0m" "$_"
+    # sudo systemctl disable --now {} && echo -e "\e[32mDisabled:\e[0m" "$_"
   fi
   if [ "$HOSTNAME" = "s2" ]; then
-    sudo systemctl restart {nginx,mysql,"$app_service"}
+    sudo systemctl restart {nginx,mysql,"$app"} && echo -e "\e[32mRestarted:\e[0m" "$_"
+    # sudo systemctl disable --now {} && echo -e "\e[32mDisabled:\e[0m" "$_"
   fi
   if [ "$HOSTNAME" = "s3" ]; then
-    sudo systemctl restart {nginx,mysql,"$app_service"}
+    sudo systemctl restart {nginx,mysql,"$app"} && echo -e "\e[32mRestarted:\e[0m" "$_"
+    # sudo systemctl disable --now {} && echo -e "\e[32mDisabled:\e[0m" "$_"
   fi
 
-  sudo sysctl -q -p /etc/sysctl.d/99-isucon.conf
+  sudo sysctl -q -p /etc/sysctl.d/99-isucon.conf && echo -e "\e[32mApplied:\e[0m" "$_"
 }
 
 function sync() {
